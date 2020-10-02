490ebf90adc9bd7c0bba4e87c6f4f8ff1e57fd7e
Andrey Beliavsky <andreybeliavsky@gmail.com>
2020-10-02 17:00:46 +0300

    'fix'

--- gulpfile.js
+++ gulpfile.js
@@ -1,76 +1,61 @@
-let gulp = require('gulp'),
-    scss = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    autoprefixer = require('gulp-autoprefixer'),
-    browserSync = require('browser-sync').create(),
-    del = require('del'),
-    gulpIf = require("gulp-if"),
-    yargs = require("yargs"),
-    gcmq = require('gulp-group-css-media-queries');
+const gulp = require('gulp');
+const scss = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const del = require('del');
+const gulpIf = require('gulp-if');
+const yargs = require('yargs');
+const gcmq = require('gulp-group-css-media-queries');
 
+const { argv } = yargs;
+const production = !!argv.production;
 
-let argv = yargs.argv,
-    production = !!argv.production;
-
-gulp.task('browser-sync', function () {
-    browserSync.init({
-        server: {
-            baseDir: 'dist/'
-        }
-    });
+gulp.task('browser-sync', () => {
+  browserSync.init({
+    server: {
+      baseDir: 'dist/',
+    },
+  });
 });
 
-gulp.task('scss', function () {
-    return gulp.src('app/scss/*.scss')
-        .pipe(gulpIf(!production, sourcemaps.init()))
-        .pipe(scss({ outputStyle: 'expanded' }).on('error', scss.logError))
-        .pipe(gulpIf(production, autoprefixer('last 10 versions')))
-        .pipe(gulpIf(!production, sourcemaps.write()))
-        .pipe(gcmq())
-        .pipe(gulp.dest('dist/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+gulp.task('scss', () => gulp.src('app/scss/*.scss')
+  .pipe(gulpIf(!production, sourcemaps.init()))
+  .pipe(scss({ outputStyle: 'expanded' }).on('error', scss.logError))
+  .pipe(gulpIf(production, autoprefixer('last 10 versions')))
+  .pipe(gulpIf(!production, sourcemaps.write()))
+  .pipe(gcmq())
+  .pipe(gulp.dest('dist/css'))
+  .pipe(browserSync.reload({ stream: true })));
+
+gulp.task('html', () => gulp.src('app/*.html')
+  .pipe(gulp.dest('dist'))
+  .pipe(browserSync.reload({ stream: true })));
+
+gulp.task('js', () => gulp.src('app/js/*.js')
+  .pipe(gulp.dest('dist/js'))
+  .pipe(browserSync.reload({ stream: true })));
+
+gulp.task('video', () => gulp.src('app/video/*')
+  .pipe(gulp.dest('dist/video')));
+
+gulp.task('clean', async () => {
+  del.sync('dist');
 });
 
-gulp.task('html', function () {
-    return gulp.src('app/*.html')
-        .pipe(gulp.dest('dist'))
-        .pipe(browserSync.reload({ stream: true }));
+gulp.task('img', () => gulp.src('app/img/**/*')
+  .pipe(gulp.dest('dist/img')));
+
+gulp.task('watch', () => {
+  gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
+  gulp.watch('app/*.html', gulp.parallel('html'));
+  gulp.watch('app/js/*.js', gulp.parallel('js'));
+  gulp.watch('app/img/**/*', gulp.parallel('img'));
 });
 
-gulp.task('js', function () {
-    return gulp.src('app/js/*.js')
-        .pipe(gulp.dest('dist/js'))
-        .pipe(browserSync.reload({ stream: true }));
-});
-
-gulp.task('video', function () {
-    return gulp.src('app/video/*')
-        .pipe(gulp.dest('dist/video'));
-});
-
-gulp.task('clean', async function () {
-    del.sync('dist');
-});
-
-gulp.task('img', function () {
-    return gulp.src('app/img/**/*')
-        .pipe(gulp.dest('dist/img'));
-});
-
-gulp.task('watch', function () {
-    gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
-    gulp.watch('app/*.html', gulp.parallel('html'));
-    gulp.watch('app/js/*.js', gulp.parallel('js'));
-    gulp.watch('app/img/**/*', gulp.parallel('img'));
-});
-
-gulp.task('default', gulp.series("clean",
-    gulp.parallel(['html', "js", "video", 'img', 'scss']),
-    gulp.parallel(['browser-sync', 'watch'])));
+gulp.task('default', gulp.series('clean',
+  gulp.parallel(['html', 'js', 'video', 'img', 'scss']),
+  gulp.parallel(['browser-sync', 'watch'])));
 
 gulp.task('prod', gulp.series('clean',
-    gulp.parallel(['html', "js", "video", 'img', 'scss'])));
-
-
+  gulp.parallel(['html', 'js', 'video', 'img', 'scss'])));
