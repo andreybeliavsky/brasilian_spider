afa070384d2c1a544020fe346967ec8ad871cda1
Andrey Beliavsky <andreybeliavsky@gmail.com>
2020-10-02 16:05:24 +0300

    'fix'

--- app/js/index.js
+++ app/js/index.js
@@ -1,21 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const slider = document.querySelector(".swiper-container");
+document.addEventListener('DOMContentLoaded', () => {
+  const slider = document.querySelector('.swiper-container');
 
-  let mySwiper = new Swiper(slider, {
+  const mySwiper = new Swiper(slider, {
     loop: true,
-    effect: "fade",
+    effect: 'fade',
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
   });
 
-  const progress = document.querySelectorAll(".progress");
+  const progress = document.querySelectorAll('.progress');
 
-  progress.forEach(function (item) {
-    let value = item.dataset.value;
-    let scale = item.querySelector(".progress__bar-scale");
-    let text = item.querySelector(".progress__value");
+  progress.forEach((item) => {
+    const value = item.dataset;
+    const scale = item.querySelector('.progress__bar-scale');
+    const text = item.querySelector('.progress__value');
 
     if (scale && text) {
       scale.style.width = value;
