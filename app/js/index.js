e603ff7fa95d1f977b7e3130bf8ca409b37a2059
Andrey Beliavsky <andreybeliavsky@gmail.com>
2020-10-05 10:17:03 +0300

    'fix'

--- app/js/index.js
+++ app/js/index.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const slider = document.querySelector('.swiper-container');
 
-  const mySwiper = new Swiper(slider, {
+  // eslint-disable-next-line no-unused-vars
+  const swiper = new Swiper(slider, {
     loop: true,
-    effect: 'fade',
+    // effect: 'fade',
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
@@ -13,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const progress = document.querySelectorAll('.progress');
 
   progress.forEach((item) => {
-    const value = item.dataset;
+    const { value } = item.dataset;
     const scale = item.querySelector('.progress__bar-scale');
     const text = item.querySelector('.progress__value');
 
